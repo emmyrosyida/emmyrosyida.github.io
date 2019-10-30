21fa7e2d6c2b498ae82bc4976000afecfd9487c6
emmyrosyida <emmyrosyida@gmail.com>
2019-10-30 10:51:05 +0800

    Hey, let's do something new?
    Start of a new life

--- testpwa/server.js
+++
@@ -1,5 +0,0 @@
-const port = process.env.PORT || 8809
-const express = require('express')
-express()
-	.use(express.static('src'))
-	.listen(port, () => console.log(`openui5 pwa sample is live on port ${port}`))
